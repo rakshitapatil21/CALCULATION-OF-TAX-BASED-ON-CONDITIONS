baa1c3ee75f5498a0ece3d14fc0de2403bd874fc
Rakshita Patil <119129522+rakshitapatil21@users.noreply.github.com>
2023-01-20 08:54:37 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+    float income,tax;
+    printf("Enter the income:");
+    scanf("%f",&income);
+    if(income<=150000)
+    {
+        tax=0;
+    }
+    else if(income>150000 && income<=300000)
+    {
+        tax=150000*0+(income-150000)*0.10;
+    }
+    else if(income>300000 && income<=500000)
+    {
+        tax=0+150000+(income-300000)*0.20;
+    }
+    else
+    {
+        tax=0+150000+40000+(income-500000)*0.30;
+    }
+    printf("TAX=%f",tax);
+}
